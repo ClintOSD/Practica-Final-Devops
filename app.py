a7f40a7a933d3bde0460844adbdabc5f5499e2c9
ClintOSD <113477414+ClintOSD@users.noreply.github.com>
2025-08-16 18:49:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '¡Hola mundo desde Docker! Mi nombre es Clint Santana 2022-0023'
+    return '¡Hola, Mundo desde un contenedor Docker!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
