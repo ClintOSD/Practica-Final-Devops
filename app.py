0c995b417a3aa8f05bcbf78107e4ec6234422bf4
ClintOSD <113477414+ClintOSD@users.noreply.github.com>
2025-08-16 18:47:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '¡Un gran saludo para todos, como les va?!, Mi nombre es Clint Santana 2022-0023'
+    return '¡Un gran saludo para todos! Mi nombre es Clint Santana 2022-0023'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
